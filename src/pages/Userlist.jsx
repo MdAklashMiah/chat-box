1e8931ae7e0aab44c78ae62fa961b15233fd4cb7
MdAklashMiah <dev.md.aklashmiah@gmail.com>
2025-07-03 04:04:32 +0600

    some change

--- src/pages/Userlist.jsx
+++ src/pages/Userlist.jsx
@@ -1,37 +1,48 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { getDatabase, ref, onValue } from "firebase/database";
 
 const Userlist = () => {
+  const [userList, setUserList] = useState([]);
   const db = getDatabase();
 
   useEffect(() => {
     const userListRef = ref(db, "userslist/");
     onValue(userListRef, (snapshot) => {
-      snapshot.forEach((item) =>{
-        console.log(item.val())
-      })
+      const array = [];
+      snapshot.forEach((item) => {
+        array.push(item.val());
+      });
+      setUserList(array);
     });
   }, []);
+
+  console.log(userList);
   return (
     <>
       <div className="relative w-md h-1/2 rounded-lg border border-slate-200 bg-white shadow-sm p-2 mt-2">
         <h1 className="text-2xl font-bold">User List</h1>
-        <div className="text-slate-800 flex w-full items-center justify-between rounded-md p-3 transition-all hover:bg-slate-100 focus:bg-slate-100 active:bg-slate-100">
-          <div className="mr-4 grid place-items-center">
-            <img
-              alt="candice"
-              src="https://docs.material-tailwind.com/img/face-1.jpg"
-              className="relative inline-block h-12 w-12 !rounded-full  object-cover object-center"
-            />
-          </div>
-          <div>
-            <h6 className="text-slate-800 font-medium">Tania Andrew</h6>
-            <p className="text-slate-500 text-sm">Software Engineer</p>
-          </div>
-          <button className="bg-blue-400 px-3 py-1 border rounded">
-            Add Friend
-          </button>
-        </div>
+        <ul>
+          {userList.map((item) => {
+            return (
+              <li className="text-slate-800 flex w-full items-center justify-between rounded-md p-3 transition-all hover:bg-slate-100 focus:bg-slate-100 active:bg-slate-100">
+                <div className="mr-4 grid place-items-center">
+                  <img
+                    alt="candice"
+                    src="https://docs.material-tailwind.com/img/face-1.jpg"
+                    className="relative inline-block h-12 w-12 !rounded-full  object-cover object-center"
+                  />
+                </div>
+                <div>
+                  <h6 className="text-slate-800 font-medium">{item.name}</h6>
+                  <p className="text-slate-500 text-sm">{item.email}</p>
+                </div>
+                <button className="bg-blue-400 px-3 py-1 border rounded">
+                  Add Friend
+                </button>
+              </li>
+            );
+          })}
+        </ul>
       </div>
     </>
   );
